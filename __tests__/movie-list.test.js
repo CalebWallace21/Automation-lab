2fde93d02c0b4e6cecb3814c3240f48e2fda3b71
Caleb Wallace <wallacekid1996@gmail.com>
2022-11-22 16:11:36 -0700

    Automation lab submission

---
+++ __tests__/movie-list.test.js
@@ -0,0 +1,33 @@
+const {Builder, Capabilities} = require('selenium-webdriver')
+
+require(`chromedriver`)
+
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+
+const {addMovie, deleteMovie, clickMovie} = require(`../functions/addMovie`)
+
+beforeAll(async () => {
+    await (await driver).get('http://127.0.0.1:5501/movieList/index.html')
+})
+
+afterAll(async () => {
+    await driver.quit()
+})
+
+test(`Add a movie`,  async () => {
+    await addMovie(driver)
+
+     await driver.sleep(3000)
+})
+
+test(`Delete a movie`,  async () => {
+    await deleteMovie(driver)
+
+     await driver.sleep(3000)
+})
+
+test(`Click a movie`,  async () => {
+    await clickMovie(driver)
+
+     await driver.sleep(3000)
+})
---
+++ functions/addMovie.js
@@ -0,0 +1,39 @@
+const {By} = require(`selenium-webdriver`)
+
+const addMovie = async (driver) => {
+    await driver.findElement(By.xpath(`//input`)).sendKeys(`The Dark Knight`)
+
+    await driver.findElement(By.xpath(`//button`)).click()
+
+    const movie = await driver.findElement(By.xpath(`//li`))
+
+    const displayed = movie.isDisplayed()
+
+    expect(displayed).toBeTruthy()
+}
+
+const deleteMovie = async(driver) => {
+    await driver.findElement(By.xpath(`//input`)).sendKeys(`A New Hope`)
+
+    await driver.findElement(By.xpath(`//button`)).click()
+
+    await driver.findElement(By.xpath(`//button[contains(text(), "x")]`)).click()
+
+}
+
+const clickMovie = async(driver)=> {
+     await driver.findElement(By.xpath(`//li`)).click()
+
+     const alert = await driver.findElement(By.xpath(`//aside`))
+
+     const displayed = alert.isDisplayed()
+     expect(displayed).toBeTruthy()
+    
+}
+
+
+module.exports = {
+    addMovie,
+    deleteMovie,
+    clickMovie
+}
